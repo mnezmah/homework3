e7d54fc04375996e19d314106f75d981aa6809fe
mnezmah <mario.nezmah@gmail.com>
2019-06-21 20:43:16 +0200

    added data to App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react'
 import logo from './logo.svg';
 import './App.css';
 import { Provider } from 'react-redux'
 import store from './store'
 
-function App() {
-  return (
-    <Provider store={store}>
-    <div className="App">
-    </div>
-    </Provider>
-  );
+class App extends Component {
+  render() {
+    const data = [
+      {
+        name: "Ivel Z3",
+        manufacturer: "Ivasim",
+        year: 1969,
+        origin: "Croatia"
+      },
+      {
+        name: "Bally Astrocade",
+        manufacturer: "Bally Consumer Products",
+        year: 1977,
+        origin: "USA"
+      },
+      {
+        name: "Sord M200 Smart Home Computer",
+        manufacturer: "Sord Computer Corporation",
+        year: 1971,
+        origin: "Japan"
+      },
+      {
+        name: "Commodore 64",
+        manufacturer: "Commodore",
+        year: 1982,
+        origin: "USA"
+      }
+    ]
+    return (
+      <Provider store={store}>
+        <div className="App">
+          <select>
+            <option value="">-- pick  a model--</option>
+            <option value="Ivel Z3">Ivel Z3 (1969)</option>
+            <option value="Bally Astrocade">Bally Astrocade (1977)</option>
+            <option value="Sord M200 Smart Home Computer">Sord M200 Smart Home Computer (1971)</option>
+          </select>
+        </div>
+      </Provider>
+    );
+  }
 }
 
 export default App;
