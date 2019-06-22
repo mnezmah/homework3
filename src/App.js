d7baf0c2a42cefb7f82c7ace56b622d43e161129
mnezmah <mario.nezmah@gmail.com>
2019-06-22 08:42:01 +0200

    turned select into a component

--- src/App.js
+++ src/App.js
@@ -1,46 +1,16 @@
 import React, { Component } from 'react'
-import logo from './logo.svg';
 import './App.css';
 import { Provider } from 'react-redux'
 import store from './store'
+import Select from './components/Select'
 
 class App extends Component {
   render() {
-    const data = [
-      {
-        name: "Ivel Z3",
-        manufacturer: "Ivasim",
-        year: 1969,
-        origin: "Croatia"
-      },
-      {
-        name: "Bally Astrocade",
-        manufacturer: "Bally Consumer Products",
-        year: 1977,
-        origin: "USA"
-      },
-      {
-        name: "Sord M200 Smart Home Computer",
-        manufacturer: "Sord Computer Corporation",
-        year: 1971,
-        origin: "Japan"
-      },
-      {
-        name: "Commodore 64",
-        manufacturer: "Commodore",
-        year: 1982,
-        origin: "USA"
-      }
-    ]
+   
     return (
       <Provider store={store}>
         <div className="App">
-          <select>
-            <option value="">-- pick  a model--</option>
-            {data.map(model => {
-              return <option value={model.name}>{model.name} ({model.year}) </option>
-            })}
-          </select>
+         <Select />
         </div>
       </Provider>
     );
